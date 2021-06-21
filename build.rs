3a9f6fa047d090c531b4b516afa14d5eaffa38a8
Daniel Porteous <danielporteous1@gmail.com>
2021-06-21 15:36:54 -0700

    Compile with std cpp 14

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
-
-
 fn main() {
     let mut builder = cxx_build::bridge("src/main.rs");
     builder
         .object("include/libs/libDellMonitorSdkLib.dylib")
+        .flag_if_supported("-std=c++14")
         .compile("dell-monitor-sdk");
 }
