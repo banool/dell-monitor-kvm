37556ab5adfef8c8f6c088309e8729280fc47f55
Daniel Porteous <danielporteous1@gmail.com>
2021-06-21 17:51:40 -0700

    Use bindgen instead of cxx

--- build.rs
+++ build.rs
@@ -1,15 +1,33 @@
+extern crate bindgen;
+
+use std::path::PathBuf;
+
+// Everything to do with linked libraries here is only for
+// checking stuff at compile time. When it comes to running
+// the binary, you have to let it know where it can find the
+// library, it is not linked into the binary. Check the README.
 fn main() {
-    // Everything to do with linked libraries here is only for
-    // checking stuff at compile time. When it comes to running
-    // the binary, you have to let it know where it can find the
-    // library, it is not linked into the binary. Check the README.
-    let mut builder = cxx_build::bridge("src/main.rs");
-    builder
-        .flag_if_supported("-std=c++14")
-        .compile("dell-monitor-sdk");
+
     // Note how the dylib name does not have the `lib` prefix.
     println!("cargo:rustc-link-lib=dylib=DellMonitorSdkLib");
     // This only tells cargo where to find the library at compile
     // time, not the binary where to find the library at runtime.
     println!("cargo:rustc-link-search=native=include/libs");
+
+    let bindings = bindgen::Builder::default()
+        .header("include/headers/UP_API.h")
+        .header("include/headers/UP_DATA.h")
+        .blocklist_type("tm")
+        .blocklist_function("SetDateTime")
+        .blocklist_function("GetDateTime")
+        .rustified_enum("monitor_code")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
-#[cxx::bridge]
-mod ffi {
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-    // TODO: Use this https://github.com/dtolnay/cxx/pull/847.
-    #[repr(i32)]
-    #[derive(Debug)]
-    enum MONITOR_CODE {
-        MONITOR_SUCCESS = 0,
-        MONITOR_FAILURE = -1,      /* General failure */
-        MONITOR_ERR_TIMEOUT = 1,   /* Function times out */
-        MONITOR_ERR_PARAMS = 2, /* Errors in parameters, out of range, invalid or unsupported modes */
-        MONITOR_ERR_CONNECT = 3, /* Errors in connecting to monitor */
-        MONITOR_ERR_COMMS = 4,  /* Errors in communicating with monitor */
-        MONITOR_ERR_STATE = 5,  /* Monitor in wrong state for API call */
-        MONITOR_ERR_NOSUPPORT = 6, /* Monitor does not support this API call */
-        MONITOR_ERR_CHECKSUM = 7, /* Checksum error */
-        MONITOR_ERR_MODULEOFF = 8, /* Error due to related module is powered off. eg) GetCalibrationModulePowerState = OFF */
-        MONITOR_ERR_INUSE = 9,     /* Monitor is currently connected to another application */
-    }
+use anyhow::{bail, Result};
 
-    unsafe extern "C++" {
-        include!("dell-monitor-kvm/include/headers/UP_API.h");
-        include!("dell-monitor-kvm/include/headers/UP_DATA.h");
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-        type MONITOR_CODE;
-
-        fn ConnectMonitor(monitor: u8) -> MONITOR_CODE;
+fn call(res: MONITOR_CODE, desc: &str) -> Result<()> {
+    match res {
+        MONITOR_CODE::MONITOR_SUCCESS => {
+            println!("Call {} succeeded", desc);
+            Ok(())
+        },
+        wildcard => bail!("Call {} failed with {:?}", desc, wildcard),
     }
 }
 
-fn main() {
-    let res = ffi::ConnectMonitor(0);
-    println!("res: {:?}", res);
+
+fn main() -> Result<()> {
+    unsafe {
+        call(Initialize(), "Initialize")?;
+    }
+
+    let num_monitors: *mut u8 = &mut 0;
+    unsafe {
+        call(GetAvailableMonitors(num_monitors), "GetAvailableMonitors" )?;
+        if *num_monitors != 2 {
+            bail!("Failed to find all monitors, only found {}", *num_monitors);
+        }
+    }
+
+    unsafe {
+        call(ConnectMonitor(1), "ConnectMonitor")?;
+    }
+
+    Ok(())
 }
