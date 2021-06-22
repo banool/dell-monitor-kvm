1399af4680563216beddca98c89e8a6b09b68919
Daniel Porteous <danielporteous1@gmail.com>
2021-06-21 18:38:22 -0700

    Print out more information

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use anyhow::{bail, Result};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+pub const NUM_EXPECTED_MONITORS: u8 = 2;
+
 fn call(res: MONITOR_CODE, desc: &str) -> Result<()> {
     match res {
         MONITOR_CODE::MONITOR_SUCCESS => {
@@ -19,20 +21,35 @@ fn call(res: MONITOR_CODE, desc: &str) -> Result<()> {
 
 fn main() -> Result<()> {
     unsafe {
+        call(SetDebugLevel(3), "SetDebugLevel")?;
+    }
+
+    unsafe {
         call(Initialize(), "Initialize")?;
     }
 
-    let num_monitors: *mut u8 = &mut 0;
     unsafe {
+        let sdk_version: *mut u16 = &mut 0;
+        call(GetVersionSDK(sdk_version), "GetVersionSDK")?;
+        println!("SDK version: {}", *sdk_version);
+    }
+
+    unsafe {
+        let num_monitors: *mut u8 = &mut 100;
         call(GetAvailableMonitors(num_monitors), "GetAvailableMonitors" )?;
-        if *num_monitors != 2 {
-            bail!("Failed to find all monitors, only found {}", *num_monitors);
+        if *num_monitors != NUM_EXPECTED_MONITORS {
+            bail!("Failed to find all {} monitors, only found {}", NUM_EXPECTED_MONITORS, *num_monitors);
         }
     }
 
     unsafe {
-        call(ConnectMonitor(1), "ConnectMonitor")?;
+        call(ConnectMonitor(0), "ConnectMonitor")?;
     }
+
+    unsafe {
+        call(DisconnectMonitor(), "DisconnectMonitor")?;
+    }
+
 
     Ok(())
 }
